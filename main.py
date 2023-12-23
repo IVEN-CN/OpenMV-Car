5648756926c5efa6e0a653523801c65c45fe92d4
IVEN-CN <13377529851@stu.gdou.edu.cn>
2023-12-23 17:39:29 +0800

    添加注释

--- main.py
+++ main.py
@@ -10,31 +10,33 @@ thresholds = [
 ]  # generic_blue_thresholds
 
 sensor.reset()
-sensor.set_pixformat(sensor.RGB565)
-sensor.set_framesize(sensor.QVGA)
-sensor.skip_frames(time=2000)
+sensor.set_pixformat(sensor.RGB565)  # 设置图像格式为RGB565
+sensor.set_framesize(sensor.QVGA)   # 设置图像大小为QVGA，
+sensor.skip_frames(time=2000)   # 使新设置生效，跳过2000帧
 sensor.set_auto_gain(False)  # 关闭自动增益
 sensor.set_auto_whitebal(False)  # 关闭自动白平衡
-ser = UART(3,9600)
-clock = time.clock()
+ser = UART(3,9600)  # 初始化串口,波特率为9600,串口3,即P4,P5
 ld1 = pyb.LED(1)
 ld3 = pyb.LED(3)
 
 def detect_color(_index):
+    """识别颜色"""
     while True:
-        clock.tick()
         img = sensor.snapshot()
         for blob in img.find_blobs(
-            [thresholds[_index]],
-            pixels_threshold=2000,
-            merge=True
+            [thresholds[_index]],   # 选择颜色,0为红色,1为绿色,2为蓝色
+            pixels_threshold=2000,  # 识别到的颜色块像素阈值
+            merge=True              # 合并颜色块
         ):
+            ld1.on()
+            ld3.on()
             if blob.rect() != None:
                 ld1.off()
                 ld3.off()
                 return 1
 
 def QR_detect():
+    """识别二维码"""
     while True:
         img = sensor.snapshot()
         img.lens_corr(1.8)
@@ -42,10 +44,11 @@ def QR_detect():
             return data.payload()
 
 def color_choice(_sign):
+    """选择颜色"""
     choices = {'11': 0, '22': 1, '33': 2}
     return choices.get(_sign[:2], 0)
 
 message = QR_detect()    # 识别二维码,返回二维码信息
 detect_color(color_choice(message))    # 识别颜色
-ser.write('1')
+ser.write('1')  # 发送信号给单片机,开始执行任务
 
